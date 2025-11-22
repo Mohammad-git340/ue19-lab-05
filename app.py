6350336c1cb5d608fad64b9be29c62e23ee7090a
Mohammad-git340 <m.hussain@student.helmo.be>
2025-11-22 20:25:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import requests
 
+def main():
+    url = "https://v2.jokeapi.dev/joke/Any"
+    response = requests.get(url)
+    data = response.json()
+
+    if data.get("type") == "single":
+        print("\nBlague :")
+        print(data["joke"])
+    else:
+        print("\nBlague :")
+        print(data["setup"])
+        print(data["delivery"])
+
+if __name__ == "__main__":
+    main()
